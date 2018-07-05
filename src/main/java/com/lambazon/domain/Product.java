e06513333b7aab8a26620aead4853dcf55f79653
daunas <sylvain.daunas@enedis-grdf.fr>
2018-07-05 15:17:13 +0200

    add function AllStockPrice= quantity * price

--- src/main/java/com/lambazon/domain/Product.java
+++ src/main/java/com/lambazon/domain/Product.java
@@ -6,6 +6,7 @@ public class Product {
 	private String name, description, details;
 	private int quantity;
 	private double price;
+    private double AllStockPrice;
 
 	public Product(int id, int quantity, double price, String name, String description) {
 		setId(id);
@@ -64,7 +65,7 @@ public class Product {
 	}
 
 	public double getInventoryPrice() {
-		// TODO Auto-generated method stub
-		return 0.0;
+        AllStockPrice= quantity * price;
+        return AllStockPrice;
 	}
 }
